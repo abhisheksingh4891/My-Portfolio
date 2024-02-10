8fc7fb8a39a0468da7c8991c14d3b4e8e9743cde
Abhishek Singh <singh2811abhishek@gmail.com>
2024-02-10 15:39:59 +0530

    resume

--- src/Components/Experience/ExpData.js
+++ src/Components/Experience/ExpData.js
@@ -1,18 +1,25 @@
 const ExpData = [
-    {
-        id: 1,
-        role: "Web Developer",
-        company: "Prodigy Infotech",
-        date: "January 2024 - Present",
-        p1: "Contributed to web development projects, specializing in React.js while leveraging HTML, CSS, and JavaScript to create responsive and dynamic user interfaces.Utilized React.js to build modular components, enhancing scalability and facilitating seamless integration of complex functionalities."
-    },
-    {
-        id: 2,
-        role: "Full Stack Web Developer Intern",
-        company: "Bharat Intern",
-        date: "November 2023 - December 2023",
-        p1: "Assisted in front end development of a web application for browser using Html, Css, ReactJs.Worked with React components, props and hooks to implement routing in the app and allow users to navigate between different pages.Utilized react router as a development environment in order to visualize the website."
-    }
+  {
+    id: 1,
+    role: "Web Developer Intern",
+    company: "Bharat Intern",
+    date: "November 2023 - December 2023",
+    p1: "Assisted in front end development of a web application for browser using Html, Css, ReactJs.Worked with React components, props and hooks to implement routing in the app and allow users to navigate between different pages.Utilized react router as a development environment in order to visualize the website.",
+  },
+  {
+    id: 2,
+    role: "Full Stack Web Developer Intern",
+    company: "Prodigy Infotech",
+    date: "November 2023 - February 2024",
+    p1: "Contributed to web development projects, specializing in React.js while leveraging HTML, CSS, and JavaScript to create responsive and dynamic user interfaces.Utilized React.js to build modular components, enhancing scalability and facilitating seamless integration of complex functionalities.",
+  },
+  {
+    id: 3,
+    role: "Full Stack Web Developer",
+    company: "360 Bytes",
+    date: "February 2024 - Present",
+    p1: "Responsible for development of the front end of a web application for browser using Html, Css, JavaScript, ReactJs. Utilized React components, props, and hooks (useState, useEffect, useContext) to optimize data flow within the app’s hierarchy.",
+  },
 ];
 
-export default ExpData
+export default ExpData;
