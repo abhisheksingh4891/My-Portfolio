8c76c64730a2db1734254670454e50226f477c7d
Abhishek Singh <singh2811abhishek@gmail.com>
2024-02-23 22:14:45 +0530

    change

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,39 +5,39 @@ import image from "./images/image.jpg";
 const Home = () => {
   return (
     <>
-    <section id="home1" className="home">
-      <div className="blur-overlay"></div> {/* Add the blur overlay */}
-      <div className="logo">
-        <img src={image} alt="ppic" />
-      </div>
-      <div className="parag">
-        <p>
-          Hello, I'm Abhishek Singh. I'm a last year undergraduate student
-          pursuing my bachelors in computer science at Chandigarh University.
-          I spend my day working on web apps with Html, JavaScript, React.js
-          and Tailwind CSS.
-        </p>
-      </div>
-      <div className="all">
-        <div className="bttn">
-          <a href="https://github.com/abhisheksingh4891">
-            <button>GitHub</button>
-          </a>
+      <section id="home1" className="home">
+        <div className="blur-overlay"></div> {/* Add the blur overlay */}
+        <div className="logo">
+          <img src={image} alt="ppic" />
         </div>
-        <div className="bttn">
-          <a href="https://www.linkedin.com/in/abhishek-singh-7a39a5219">
-            <button>LinkedIn</button>
-          </a>
+        <div className="parag">
+          <p>
+            Hello, I'm Abhishek Singh. I'm a last year undergraduate student
+            pursuing my bachelors in computer science at Chandigarh University.
+            I spend my day working on web apps with Html, JavaScript, React.js
+            and Tailwind CSS.
+          </p>
         </div>
-        <div className="bttn">
-          <a href="Resume.pdf" download="Abhishek resume">
-            <button>Resume</button>
-          </a>
+        <div className="all">
+          <div className="bttn">
+            <a href="https://github.com/abhisheksingh4891">
+              <button>GitHub</button>
+            </a>
+          </div>
+          <div className="bttn">
+            <a href="https://www.linkedin.com/in/abhishek-singh-7a39a5219">
+              <button>LinkedIn</button>
+            </a>
+          </div>
+          <div className="bttn">
+            <a href="Resume.pdf" download="Abhishek resume">
+              <button>Resume</button>
+            </a>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
       <div className="verticalv"></div>
-      </>
+    </>
   );
 };
 
